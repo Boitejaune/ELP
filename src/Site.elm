4943097f821711e6555e867a43b01c22095c66ac
Wassim <jahidwassim@hotmail.com>
2025-01-09 10:44:17 +0100

    Site

---
+++ src/Site.elm
@@ -0,0 +1,52 @@
+module Site exposing (main,init,update,view)
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  }
+
+
+init : Model
+init =
+  { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Ecrire votre dictée", value model.content, onInput Change ] []
+    ]
