3318ba0480535641a5347191fea184739c2e5443
Wassim <jahidwassim@hotmail.com>
2025-01-09 09:00:05 +0100

    site/main go

--- dictee.go
+++ dictee.go
@@ -5,11 +5,21 @@ import (
     "io/ioutil"
 )
 
-func main() {
+
+
+func read() {
+
     data, err := ioutil.ReadFile("dictée.txt") // lire le fichier text.txt
-    if err != nil {
+    if err != nil {		// si il y a une erreur 
         fmt.Println(err)
     }
 
     fmt.Println(string(data)) // conversion de byte en string
+
+}
+
+func main() {
+
+	
+
 }
